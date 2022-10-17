54ef35ac26447f4bcfde668aa10cae925a286e48
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-17 15:21:31 +0700

    1234

--- flutter1/lib/src/pages/home/home_page.dart
+++ flutter1/lib/src/pages/home/home_page.dart
@@ -38,25 +38,46 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(children: [
-        _buildProfile()
-      ],),
+      child: Column(
+        children: [
+          _buildProfile(),
+          ListTile(
+            onTap: (){},
+            title: Text("BarCode"),
+            leading: const Icon(Icons.code_rounded, color: Colors.deepOrange),
+          ),
+          ListTile(
+            onTap: (){},
+            title: Text("QRCode"),
+            leading: Icon(Icons.qr_code, color: Colors.green),
+          ),
+          ListTile(
+            onTap: (){},
+            title: Text("Scanner"),
+            leading: const Icon(Icons.qr_code_scanner, color: Colors.blueGrey),
+          ),
+          ListTile(
+            onTap: (){},
+            title: Text("Map"),
+            leading: Icon(Icons.map_outlined, color: Colors.blue),
+          ),
+
+        ],
+      ),
     );
   }
 
-
   UserAccountsDrawerHeader _buildProfile() => UserAccountsDrawerHeader(
-    currentAccountPicture: Container(
-      child: const CircleAvatar(
-        backgroundImage: NetworkImage('https://cdn-images-1.medium.com/max/280/1*X5PBTDQQ2Csztg3a6wofIQ@2x.png'),
-      ),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        shape: BoxShape.circle,
-      ),
-    ),
-    accountName: Text('CMDev'),
-    accountEmail: Text('support@codemobiles.com'),
-  );
-
+        currentAccountPicture: Container(
+          child: const CircleAvatar(
+            backgroundImage: NetworkImage('https://cdn-images-1.medium.com/max/280/1*X5PBTDQQ2Csztg3a6wofIQ@2x.png'),
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+          ),
+        ),
+        accountName: Text('CMDev'),
+        accountEmail: Text('support@codemobiles.com'),
+      );
 }
