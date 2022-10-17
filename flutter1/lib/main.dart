9c8f0623c4777e7f7569ba19f0e2374e4d26a5ed
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-17 11:08:45 +0700

    1234

--- flutter1/lib/main.dart
+++ flutter1/lib/main.dart
@@ -1,47 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter1/src/app.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Workshop'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("CodeMobiles"),),
-      body: Center(child: Text("1234")),
-    );
-  }
-}
---
+++ flutter1/lib/src/pages/home/home_page.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CodeMobiles"),
+      ),
+      body: Center(
+        child: Text("1234"),
+      ),
+    );
+  }
+}
