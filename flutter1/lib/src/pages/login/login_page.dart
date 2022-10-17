d92ef1876c0258728008f8139e957e9b09c6c636
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-17 11:21:20 +0700

    1234

--- flutter1/lib/src/pages/login/login_page.dart
+++ flutter1/lib/src/pages/login/login_page.dart
@@ -10,6 +10,11 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Text("Login");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Text("Login"),
+    );
   }
 }
