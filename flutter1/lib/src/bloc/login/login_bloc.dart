a631552818140b9e191fb1e0e931f57a23e1e5cc
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-18 11:34:35 +0700

    1234

--- flutter1/lib/src/bloc/login/login_bloc.dart
+++ flutter1/lib/src/bloc/login/login_bloc.dart
@@ -10,8 +10,14 @@ part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   LoginBloc() : super(LoginState()) {
-    on<LoginEvent>((event, emit) {
-      // TODO: implement event handler
+    // Register
+    on<LoginEvent_Register>((event, emit) {
+      print("Register: " + event.payload.username + "," + event.payload.password);
+    });
+
+    // Login
+    on<LoginEvent_Login>((event, emit) {
+      print("Register: " + event.payload.username + "," + event.payload.password);
     });
   }
 }
