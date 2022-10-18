a3dbb395f06fbc007d6c4aa48a0884adbb2d5ab0
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-18 14:09:11 +0700

    1234

--- flutter1/lib/src/bloc/home/home_state.dart
+++ flutter1/lib/src/bloc/home/home_state.dart
@@ -1,14 +1,17 @@
 part of 'home_bloc.dart';
 
 class HomeState extends Equatable {
-
-  HomeState({this.count = 0}){}
+  HomeState({this.count = 0, this.isGrid = true}) {}
   final int count;
+  final bool isGrid;
 
-  HomeState copyWith({int? count}){
-    return HomeState(count: count ?? this.count);
+  HomeState copyWith({int? count, bool? isGrid}) {
+    return HomeState(
+      count: count ?? this.count,
+      isGrid: isGrid ?? this.isGrid,
+    );
   }
 
   @override
-  List<Object> get props => [count];
+  List<Object> get props => [count, isGrid];
 }
