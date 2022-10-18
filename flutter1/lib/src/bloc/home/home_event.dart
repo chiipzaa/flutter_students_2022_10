3160e0c71c727cd2c3bec86913ff5d581cdaa206
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-18 09:53:14 +0700

    1234

--- flutter1/lib/src/bloc/home/home_event.dart
+++ flutter1/lib/src/bloc/home/home_event.dart
@@ -1,5 +1,13 @@
 part of 'home_bloc.dart';
 
 abstract class HomeEvent extends Equatable {
-  const HomeEvent();
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
 }
+
+// Add Event
+class HomeEvent_Add extends HomeEvent {}
+
+// Delete Event
+class HomeEvent_Delete extends HomeEvent {}
