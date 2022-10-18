e4e89b8be32d991110ac64501250f50e12b94764
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-18 10:16:27 +0700

    1234

--- flutter1/lib/src/app.dart
+++ flutter1/lib/src/app.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter1/src/bloc/home/home_bloc.dart';
+import 'package:flutter1/src/bloc/login/login_bloc.dart';
 import 'package:flutter1/src/pages/app_routes.dart';
 import 'package:flutter1/src/pages/home/home_page.dart';
 import 'package:flutter1/src/pages/login/login_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CMApp extends StatelessWidget {
   const CMApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    final homeBloc = BlocProvider<HomeBloc>(create: (context) => HomeBloc());
+    final loginBloc = BlocProvider<LoginBloc>(create: (context) => LoginBloc());
+    
+    return MultiBlocProvider(
+      providers: [homeBloc,loginBloc],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routes: AppRoute.all,
+        home: LoginPage(),
       ),
-      routes: AppRoute.all,
-      home: LoginPage(),
     );
   }
 }
