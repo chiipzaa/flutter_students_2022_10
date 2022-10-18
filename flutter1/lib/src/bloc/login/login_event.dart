dfc432130226b3a1609964fc2caf0cfa037c88ee
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-18 11:20:17 +0700

    1234

--- flutter1/lib/src/bloc/login/login_event.dart
+++ flutter1/lib/src/bloc/login/login_event.dart
@@ -1,5 +1,11 @@
 part of 'login_bloc.dart';
 
 abstract class LoginEvent extends Equatable {
-  const LoginEvent();
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class LoginEvent_Register extends LoginEvent {
+
 }
