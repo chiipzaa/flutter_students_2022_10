887dbb7fd75acf4883706aae1a1c6c7cc6a47d89
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-18 15:33:22 +0700

    1234

--- flutter1/lib/src/pages/home/home_page.dart
+++ flutter1/lib/src/pages/home/home_page.dart
@@ -18,7 +18,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-     NetworkService().getProducts();
+    NetworkService().getProducts();
     super.initState();
   }
 
