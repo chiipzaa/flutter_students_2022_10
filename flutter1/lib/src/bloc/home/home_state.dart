fceadce1f2eecddfe38af9cca05895ab9e5930e2
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-18 10:08:02 +0700

    1234

--- flutter1/lib/src/bloc/home/home_state.dart
+++ flutter1/lib/src/bloc/home/home_state.dart
@@ -1,11 +1,12 @@
 part of 'home_bloc.dart';
 
 class HomeState extends Equatable {
-  HomeState({this.count = 0}){}
-  int count = 0;
 
-  HomeState copyWith(int count){
-    return HomeState(count: )
+  HomeState({this.count = 0}){}
+  final int count;
+
+  HomeState copyWith({int? count}){
+    return HomeState(count: count ?? this.count);
   }
 
   @override
