c664a08452ef1f04077386f62efb2f9be53993ce
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-19 11:44:12 +0700

    1234

--- flutter1/lib/src/pages/management/management_page.dart
+++ flutter1/lib/src/pages/management/management_page.dart
@@ -1,15 +1,25 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter1/src/models/Product.dart';
 
 class ManagementPage extends StatefulWidget {
   const ManagementPage({Key? key}) : super(key: key);
-
   @override
   State<ManagementPage> createState() => _ManagementPageState();
 }
 
 class _ManagementPageState extends State<ManagementPage> {
+  final _form = GlobalKey<FormState>();
+  var _product = Product(name: "", price: 0, stock: 0);
+  var _editMode = false;
+  File? _imageFile;
+
+
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Managment"),
