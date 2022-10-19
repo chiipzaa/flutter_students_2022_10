7cbb7fa4b3584587414019ade3e75205865080e6
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-19 11:25:35 +0700

    1234

--- flutter1/lib/src/services/network_service.dart
+++ flutter1/lib/src/services/network_service.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter1/src/constants/network_api.dart';
 import 'package:flutter1/src/models/Product.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http_parser/http_parser.dart';
 
 class NetworkService {
   NetworkService._internal();
@@ -33,61 +36,73 @@ class NetworkService {
     );
 
   getProducts() async {
-    final tmp = Dio();
-    tmp.interceptors.add(
-      InterceptorsWrapper(
-        onRequest: (options, handler) {
-          options.baseUrl = NetworkAPI.baseURL;
-          return handler.next(options);
-        },
-        onResponse: (response, handler) async {
-          // await Future.delayed(Duration(seconds: 10));
-          return handler.next(response);
-        },
-        onError: (DioError e, handler) {
-          switch (e.response?.statusCode) {
-            case 301:
-              break;
-            case 401:
-              break;
-            default:
-          }
-          return handler.next(e);
-        },
-      ),
-    );
-    final finalTmp = tmp;
-
-    final tmpV2 = Dio()
-    ..interceptors.add(
-      InterceptorsWrapper(
-        onRequest: (options, handler) {
-          options.baseUrl = NetworkAPI.baseURL;
-          return handler.next(options);
-        },
-        onResponse: (response, handler) async {
-          // await Future.delayed(Duration(seconds: 10));
-          return handler.next(response);
-        },
-        onError: (DioError e, handler) {
-          switch (e.response?.statusCode) {
-            case 301:
-              break;
-            case 401:
-              break;
-            default:
-          }
-          return handler.next(e);
-        },
-      ),
-    );
-
-
 
     final response = await _dio.get(NetworkAPI.product);
     if (response.statusCode == 200){
         final result = productFromJson(jsonEncode(response.data));
         return result;
     }
+    throw Exception();
+  }
+
+
+  Future<String> editProduct(Product product, {File? imageFile}) async {
+    FormData data = FormData.fromMap({
+      'name': product.name,
+      'price': product.price,
+      'stock': product.stock,
+      if (imageFile != null)
+        'photo': await MultipartFile.fromFile(
+          imageFile.path,
+          contentType: MediaType('image', 'jpg'),
+        ),
+    });
+
+    final response =
+    await _dio.put('${NetworkAPI.product}/${product.id}', data: data);
+    if (response.statusCode == 200) {
+      return 'Edit Successfully';
+    }
+    throw Exception();
+  }
+
+  Future<String> addProduct(Product product, {File? imageFile}) async {
+    FormData data = FormData.fromMap({
+      'name': product.name,
+      'price': product.price,
+      'stock': product.stock,
+      if (imageFile != null)
+        'photo': await MultipartFile.fromFile(
+          imageFile.path,
+          contentType: MediaType('image', 'jpg'),
+        ),
+    });
+
+    final response = await _dio.post(NetworkAPI.product, data: data);
+    if (response.statusCode == 201) {
+      return 'Add Successfully';
+    }
+    throw Exception();
+  }
+
+  Future<String> submitLocation(LatLng position) async {
+    var params = {
+      "lat": position.latitude,
+      "lng": position.longitude,
+    };
+
+    Response response = await Dio().post(
+      "http://192.168.1.224:1150/submit_location",
+      options:
+      Options(headers: {HttpHeaders.contentTypeHeader: "application/json"}),
+      data: jsonEncode(params),
+    );
+
+    if (response.statusCode == 201) {
+      return 'Submit Successfully';
+    }else{
+      return 'Submit Failed';
+    }
+
   }
 }
