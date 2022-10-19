4f3ada42e95f6a6b3ca4780c68fc78826d7aa633
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-19 09:46:03 +0700

    1234

--- flutter1/lib/src/pages/home/home_page.dart
+++ flutter1/lib/src/pages/home/home_page.dart
@@ -6,6 +6,8 @@ import 'package:flutter1/src/services/network_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../../constants/network_api.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
 
@@ -16,7 +18,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   void initState() {
     context.read<HomeBloc>().add(HomeEvent_Fetch());
@@ -48,8 +49,10 @@ class _HomePageState extends State<HomePage> {
       drawer: CustomDrawer(),
       body: BlocBuilder<HomeBloc, HomeState>(
         builder: (context, state) {
-          if (state.status != FetchStatus.success){
-            return Center(child: Text("Loading"),);
+          if (state.status != FetchStatus.success) {
+            return Center(
+              child: Text("Loading"),
+            );
           }
 
           final products = state.products;
@@ -93,11 +96,22 @@ class _HomePageState extends State<HomePage> {
 
   _showListView(List<Product> products) {
     return ListView.builder(
-      itemCount: 30,
+      itemCount: products.length,
       itemBuilder: (context, index) {
-        return Text(
-          "1234",
-          style: TextStyle(fontSize: 20),
+        return Padding(
+          padding: const EdgeInsets.only(top: 12.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 200,
+                child: Image.network(NetworkAPI.imageURL + "/" + products[index].image!),
+              ),
+              Text(
+                products[index].name!,
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
         );
       },
     );
@@ -115,7 +129,10 @@ class _HomePageState extends State<HomePage> {
         return Container(
           color: Colors.red,
           child: Center(
-            child: Text(products[index].name!, style: TextStyle(color: Colors.white),),
+            child: Text(
+              products[index].name!,
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         );
       },
@@ -160,20 +177,17 @@ class CustomDrawer extends StatelessWidget {
     );
   }
 
-  Builder _buildLogoutButton() =>
-      Builder(
-        builder: (context) =>
-            SafeArea(
-              child: ListTile(
-                leading: FaIcon(FontAwesomeIcons.signOutAlt),
-                title: Text('Log out'),
-                onTap: () => context.read<LoginBloc>().add(LoginEvent_Logout()),
-              ),
-            ),
+  Builder _buildLogoutButton() => Builder(
+        builder: (context) => SafeArea(
+          child: ListTile(
+            leading: FaIcon(FontAwesomeIcons.signOutAlt),
+            title: Text('Log out'),
+            onTap: () => context.read<LoginBloc>().add(LoginEvent_Logout()),
+          ),
+        ),
       );
 
-  UserAccountsDrawerHeader _buildProfile() =>
-      UserAccountsDrawerHeader(
+  UserAccountsDrawerHeader _buildProfile() => UserAccountsDrawerHeader(
         currentAccountPicture: Container(
           child: const CircleAvatar(
             backgroundImage: NetworkImage('https://cdn-images-1.medium.com/max/280/1*X5PBTDQQ2Csztg3a6wofIQ@2x.png'),
--- flutter1/readme_fix_errors.txt
+++ flutter1/readme_fix_errors.txt
@@ -10,3 +10,13 @@
 
 # minSDK error in android/app/build.gradle
    minSdkVersion localProperties.getProperty('flutter.minSdkVersion')
+
+
+# fix certification error
+https://stackoverflow.com/questions/70536488/flutter-dio-https-certificate-validation-ignoring-onbadcertificate-ca-cert-pro
+
+//check bad certificate
+    (_dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (HttpClient client) {
+    client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+    return client;
+    };
