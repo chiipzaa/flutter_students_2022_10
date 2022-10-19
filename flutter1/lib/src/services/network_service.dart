d2c232e258f43f2797f1d4fff0334e7bef82b1f6
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-19 11:07:26 +0700

    1234

--- flutter1/lib/src/services/network_service.dart
+++ flutter1/lib/src/services/network_service.dart
@@ -1,16 +1,39 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
+import 'package:flutter1/src/constants/network_api.dart';
 import 'package:flutter1/src/models/Product.dart';
 
 class NetworkService {
   NetworkService._internal();
   static final NetworkService _instance = NetworkService._internal();
   factory NetworkService() => _instance;
-  static final Dio _dio = Dio();
+  static final Dio _dio = Dio()
+    ..interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          options.baseUrl = NetworkAPI.baseURL;
+          return handler.next(options);
+        },
+        onResponse: (response, handler) async {
+          // await Future.delayed(Duration(seconds: 10));
+          return handler.next(response);
+        },
+        onError: (DioError e, handler) {
+          switch (e.response?.statusCode) {
+            case 301:
+              break;
+            case 401:
+              break;
+            default:
+          }
+          return handler.next(e);
+        },
+      ),
+    );
 
   getProducts() async {
-    final response = await _dio.get("https://cmcrud.herokuapp.com/products");
+    final response = await _dio.get(NetworkAPI.product);
     if (response.statusCode == 200){
         final result = productFromJson(jsonEncode(response.data));
         return result;
