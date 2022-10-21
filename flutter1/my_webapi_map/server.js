3d5736d571470e9cc7ffa157e44e91ba837c923e
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-10-21 09:33:26 +0700

    1234

--- flutter1/my_webapi_map/server.js
+++ flutter1/my_webapi_map/server.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+// curl localhost:3000
+app.get("/", (req, res)=>{
+  res.send("I am root");
+});
+
+// curl "localhost:3000/getProfile?username=admin"
+app.get("/getProfile", (req, res)=>{
+  res.json(req.query);
+});
+
+// curl "localhost:3000/register"
+app.post("/register", (req, res)=>{
+  res.json(req.body);
+});
+
 
 app.listen(3000,()=>console.log("Server is ready"));
